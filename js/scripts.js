76e265fd9a3545ffa7c4355dab2b169c219ba7e0
Hubert Maćkowiak <mackowiak.h@wp.pl>
2017-11-03 20:00:29 +0100

    final correction in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 var a = prompt("Eneter number a");
 var b = prompt("Eneter number b");
 var value = (a * a) - (2 * a * b) + (b * b);
-alert("value is equal to  "+value)
-console.log("value is equal to  "+ value);
-if (+ value > 0 ) {
+alert("value is equal to  " + value)
+console.log("value is equal to  " + value);
+if (value > 0) {
 	console.log('value is positive');
-} else if (+ value < 0 ) {
+} else if (value < 0) {
 	console.log('value is negative');
-}
-if (+value == 0) {
+} else if (value == 0) {
 	console.log('value is equal to zero');
-} else if (+ value != 0) {
-	console.log('value is not equal to zero');
+} else {
+	console.log('value is not a number');
 }
